84ba025b9ccab09497a5b248dba8031b100c38d5
bnmahajan78 <bnmahajan78@gmail.com>
2022-05-09 23:27:18 +0530

    second commit

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace vuedemobackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImageController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ImageController> _logger;
+
+        public ImageController(ILogger<ImageController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetImage")]  /// Get[NameofController]
+        public IEnumerable<ImageData> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new ImageData
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
